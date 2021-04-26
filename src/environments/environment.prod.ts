9280919cf36d3662c370d4450444061790bf427c
ahmetbozdoan <ahmetbozdoan@gmail.com>
2021-04-26 03:04:11 +0300

    npm build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBiX-qIPmZt8I-TOGFr3Q9OpMiBqdUEi00",
+    authDomain: "angular11-card.firebaseapp.com",
+    databaseURL: "https://angular11-card-default-rtdb.firebaseio.com",
+    projectId: "angular11-card",
+    storageBucket: "angular11-card.appspot.com",
+    messagingSenderId: "861068153849",
+    appId: "1:861068153849:web:300756d2188ad87b42a328",
+    measurementId: "G-FWWD33VYBZ"
+  }
 };
